96cb219414935de003d780f63241530518e0378b
KojimaNaoyuki <programkoji213@gmail.com>
2022-12-01 00:41:54 +0900

    push

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,13 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # docker-compose.ymlで指定した値かその値がNullの場合は、空文字を入れる（エラーにならないように）
+    origins ENV["ACCESS_CONTROL_ALLOW_ORIGIN"] || ""
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
